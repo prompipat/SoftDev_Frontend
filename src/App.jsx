b819a8e10ca1339f89b9b742505af563a1a4f4ba
YwThammatron <thammatron135@gmail.com>
2025-09-11 03:01:37 +0700

    Yew Edit 3

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <p>ยินดีต้อนรับสู่แอปพลิเคชันของเรา</p>
       <Search className="bg-gradient" />
       <Button className="bg-gradient text-white">คลิกทำไม</Button>
-      <h1>Homepage FrontEnd 2</h1>
+      <h1>Homepage FrontEnd 3</h1>
     </div>
   );
 }
