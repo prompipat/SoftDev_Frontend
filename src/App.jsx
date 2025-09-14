26d4d76c21074243f795e32febd5c8f002b4e095
PrompipatJa <prompipat18348@gmail.com>
2025-09-14 12:54:55 +0700

    install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import CustomerHomepage from "./pages/CustomerHomepage";
-import React, { lazy, Suspense } from "react";
-import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
-
-const LazyCustomerHomepage = lazy(() => import("./pages/CustomerHomepage"));
-
-function MainContent() {
-  const navigate = useNavigate();
-
-  const goToHomepage = () => {
-    navigate("/customer-home");
-  };
-
-  return (
-    <div>
-      <p className="logo">CaterLink</p>
-      <h1>เปรียบเทียบร้านอาหาร</h1>
-      <h2>เข้าสู่ระบบ</h2>
-      <h3>ร้านแนะนำจาก CaterLink</h3>
-      <p>ยินดีต้อนรับสู่แอปพลิเคชันของเรา</p>
-      <Search className="bg-gradient" />
-      <Button className="bg-gradient text-white" onClick={goToHomepage}>
-        ไปหน้าหลัก
-      </Button>
-      <h1>Homepage FrontEnd Really Last Time</h1>
-    </div>
-  );
-}
-
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/customer-home" element={<CustomerHomepage />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <>
+      <h1 className="logo">CaterLink</h1>
+      <h1>Welcome to CaterLink</h1>
+      <h2>ยินดีต้อนรับสู่แอปพลิเคชันของเรา</h2>
+      <div className="bg-gradient w-16 h-16"></div>
+    </>
   );
 }
 
