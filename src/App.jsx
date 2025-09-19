e3ffcfeb728a5b474db99715d5d40114a39add25
YwThammatron <thammatron135@gmail.com>
2025-09-20 03:10:15 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ui/scrolltotop";
 import CustomerHomepage from "./pages/CustomerHomepage";
 import CustomerRestaurant from "./pages/CustomerRestaurant";
+import Setting from "./pages/Setting";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<CustomerHomepage />} />
         <Route path="/customerrestaurant" element={<CustomerRestaurant />} />
+        <Route path="/setting" element={<Setting />}/>
         <Route
           path="/welcome"
           element={
